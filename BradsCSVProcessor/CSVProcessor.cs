7f7f319b733b093dccfcae16c0febab3af90420f
GIRU-GIRU <GIRU-GIRU@users.noreply.github.com>
2018-11-18 01:35:19 +0000

    Initial Commit

---
+++ BradsCSVProcessor/CSVProcessor.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace BradsCSVProcessor
+{
+    public static class CSVProcessor
+    {
+        public static List<string> ProcessCSVFile(string fileString)
+        {
+            using (TextWriter log = new StreamWriter("log"))
+            {
+                log.WriteLine("------ begin file process -------");
+                var indexofCommaList = new List<int>();
+                for (int i = 0; i < fileString.Length; i++)
+                {
+                    if (fileString[i] == ',')
+                    {
+                        indexofCommaList.Add(i);
+                    }
+                }
+
+                int[] commaLocations = indexofCommaList.ToArray();
+                int firstRowLocation = commaLocations.Skip(3).FirstOrDefault();
+
+                var firstRow = fileString.Substring(0, firstRowLocation);
+                firstRow = "\"" + firstRow + "\"";
+
+                var restOfCSV = fileString;
+                var restOfCommaLocations = commaLocations.Skip(3).ToArray();
+                commaLocations = indexofCommaList.Skip(3).ToArray();
+
+                List<string> csvRows = new List<string>();
+                csvRows.Add(firstRow);
+
+
+                List<int> everyRow = new List<int>();
+                for (int i = 0; i < restOfCommaLocations.Length; i++)
+                {
+                    if (i % 14 == 0)
+                    {
+                        everyRow.Add(restOfCommaLocations[i]);
+                    }
+                }
+
+                log.WriteLine("rows are inbetween: " + string.Join(", ", everyRow));
+
+                int end = 1;
+                for (int start = 0; start < everyRow.Count; start++)
+                {
+                    if (end == everyRow.Count)
+                    {
+                        break;
+                    }
+                    var endSkipComma = everyRow[end] - (everyRow[start] - 1);
+                    var itemToAdd = "\"" + restOfCSV.Substring(everyRow[start] + 1, endSkipComma) + "\"";
+                    csvRows.Add(itemToAdd);
+                    log.WriteLine($"\n {itemToAdd}");
+                    end++;
+                }
+                log.WriteLine("------ end file process ------- \n \n \n");
+                return csvRows;
+            }
+        }
+
+
+
+        public static bool StoreCSV(string filePath, List<string> csvRows)
+        {
+            try
+            {
+                var outputName = Path.Combine(Environment.CurrentDirectory, "output", Path.GetFileName(filePath));
+
+                using (TextWriter tw = new StreamWriter(outputName))
+                {
+                    foreach (var item in csvRows)
+                    {
+                        tw.WriteLine(item);
+                    }
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                return false;
+            }
+
+        }
+
+        public static void CheckOrCreateOutputDirectory()
+        {
+            if (!Directory.Exists("output"))
+            {
+                try
+                {
+                    System.IO.Directory.CreateDirectory("output");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Unable to create or find Output folder, please create one within the application context");
+                    Console.WriteLine("Press any key to exit");
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+    }
+}
---
+++ BradsCSVProcessor/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace BradsCSVProcessor
+{
+    class Program
+    {
+        public static List<String> csvRows;
+        public static string[] filePaths;
+        static void Main(string[] args)
+        {
+            Flavour.ConfigureConsole();
+            CSVProcessor.CheckOrCreateOutputDirectory();
+
+            Thread.Sleep(600);
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("\n \n   Enter file path \n");
+            var folderLocation = Console.ReadLine();
+            Console.WriteLine("Starting .. \n");
+            try
+            {
+               filePaths = Directory.GetFiles(folderLocation, "*.txt",
+                                    SearchOption.TopDirectoryOnly);
+            }
+            catch (Exception ex)
+            {
+                Console.Clear();
+                Console.WriteLine("Something's gone wrong.. \n \n" + ex.Message);
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            int i = 0;
+            foreach (var file in filePaths)
+            {               
+                try
+                {
+                    csvRows = CSVProcessor.ProcessCSVFile(File.ReadAllText(file));
+                    Console.WriteLine($"Processed {file}");
+                    CSVProcessor.StoreCSV(file, csvRows);
+                    Console.WriteLine($"Stored {Path.GetFileName(file)} \n");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error converting {file}: " + ex.Message);
+                    throw;
+                }
+                i++;
+            }
+
+            Console.WriteLine($"\n\nProcessed {i} documents successfully ! :)");
+            Console.WriteLine("Files will be in your output folder");
+            Console.ReadKey();
+        }
+    }
+}
